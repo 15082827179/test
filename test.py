d6cf1cd0ef3371098f2869e0c8c8b55637196576
xieys <xieys@xiaopeng.com>
2019-04-28 16:25:08 +0800

    添加项目文件。

---
+++ test.py
@@ -0,0 +1,25 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+x = np.arange(-5, 5, 0.02)
+y = np.sin(x)
+
+
+
+plt.axis([-np.pi, np.pi, -2, 2])
+
+plt.plot(x, y, color="r", linestyle="-", linewidth=1)
+
+
+plt.show()
+
+
+
+
+
+
